49d611ad42a442ac8bae8f50e41a182b5fd28536
Marcos Porto Mariño <php@marcospor.to>
2026-01-10 15:21:57 +0100

    Add aditional HTTP methods

--- src/Rodas/Psr/Http/Message/RequestMethod.php
+++ src/Rodas/Psr/Http/Message/RequestMethod.php
@@ -23,18 +23,65 @@ use function strtoupper;
 
 /**
  * Defines cases for common HTTP request methods.
+ *
+ * @link https://datatracker.ietf.org/doc/html/rfc7231#section-4.3
  */
 enum RequestMethod: string {
+    /**
+     * The HEAD method asks for a response identical to a GET request, but without a response body.
+     */
     case HEAD    = 'HEAD';
+    /**
+     * The GET method requests a representation of the specified resource.
+     * Requests using GET should only retrieve data and should not contain a request content.
+     */
     case GET     = 'GET';
+    /**
+     * The POST method submits an entity to the specified resource,
+     * often causing a change in state or side effects on the server.
+     */
     case POST    = 'POST';
+    /**
+     * The PUT method replaces all current representations of the target resource with the request content.
+     */
     case PUT     = 'PUT';
+    /**
+     * The PATCH method applies partial modifications to a resource.
+     */
     case PATCH   = 'PATCH';
+    /**
+     * The DELETE method deletes the specified resource.
+     */
     case DELETE  = 'DELETE';
+    /**
+     * The PURGE method requests a response identical to a GET request,
+     * but discarding cached versions of the resource.
+     */
     case PURGE   = 'PURGE';
+    /**
+     * The OPTIONS method describes the communication options for the target resource.
+     */
     case OPTIONS = 'OPTIONS';
+    /**
+     * The TRACE method performs a message loop-back test along the path to the target resource.
+     */
     case TRACE   = 'TRACE';
+    /**
+     * The CONNECT method establishes a tunnel to the server identified by the target resource.
+     */
     case CONNECT = 'CONNECT';
+
+    // WebDAV methods
+    case PROPFIND  = 'PROPFIND';
+    case PROPPATCH = 'PROPPATCH';
+    case MKCOL     = 'MKCOL';
+    case COPY      = 'COPY';
+    case MOVE      = 'MOVE';
+    case LOCK      = 'LOCK';
+    case UNLOCK    = 'UNLOCK';
+
+    // BITS method
+    case BITS_POST = 'BITS_POST';
 
     /**
      * Returns a RequestMethod case from a string
